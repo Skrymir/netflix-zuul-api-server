f10e75f1b0198742cf53b80a00f3a8203eea82f1
Joshua Colbeck <skrymir@gmail.com>
2018-06-15 13:44:45 -0500

    add sleuth and zipkin support

--- src/main/java/com/skrymir/microservices/netflixzuulapiserver/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/skrymir/microservices/netflixzuulapiserver/NetflixZuulApiGatewayServerApplication.java
@@ -1,9 +1,11 @@
 package com.skrymir.microservices.netflixzuulapiserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -13,4 +15,9 @@ public class NetflixZuulApiGatewayServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
 	}
+
+	@Bean
+    public Sampler defaultSampler() {
+	    return Sampler.ALWAYS_SAMPLE;
+    }
 }
